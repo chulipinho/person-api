3a69610b13bf98fbf921780dde9b4a7d65b4d882
chulipinho <fellipe.luz.machado@gmail.com>
2023-02-13 19:43:36 -0300

    Remove useless code

--- data/person.go
+++ data/person.go
@@ -1,7 +1,5 @@
 package data
 
-import "log"
-
 type Person struct {
 	Id      int    `json:"id"`
 	Name    string `json:"name"`
@@ -10,12 +8,3 @@ type Person struct {
 }
 
 type People []*Person
-
-type PersonDB struct {
-	db PersonDAO
-	l  log.Logger
-}
-
-func NewPersonDB(db PersonDAO, l log.Logger) *PersonDB {
-	return &PersonDB{db, l}
-}
