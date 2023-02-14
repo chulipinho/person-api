1decda99528d5facef4f609411bd8074a372e45e
Chulipinho <fellipe.luz.machado@gmail.com>
2023-02-13 21:06:18 -0300

    Create main func

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/chulipinho/person-api/data"
+	"github.com/chulipinho/person-api/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	l := log.New(os.Stdout, "person-api", log.LstdFlags)
+	db := data.NewMock()
+
+	ph := handlers.NewPersonHandler(db, l)
+
+	r := mux.NewRouter()
+
+	getR := r.Methods(http.MethodGet).PathPrefix("/person/").Subrouter()
+	getR.HandleFunc("/", ph.GetAll)
+	getR.HandleFunc("/{id}", ph.GetById)
+
+	server := http.Server{
+		Addr:         ":1234",           // configure the bind address
+		Handler:      r,                 // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	l.Println("Starting server on port 1234")
+
+	err := server.ListenAndServe()
+	if err != nil {
+		l.Fatalf("Error starting server: %s\n", err)
+	}
+}
