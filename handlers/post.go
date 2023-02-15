c857e878b9ba98cfb221ffd51055ea5d6acbfb73
chulipinho <fellipe.luz.machado@gmail.com>
2023-02-15 15:12:04 -0300

    Handle post exceptions

--- handlers/post.go
+++ handlers/post.go
@@ -7,8 +7,14 @@ import (
 )
 
 func (h *PersonHandler) Post(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("[DEBUG] Posting new person")
 	var person data.Person
 
 	data.FromJSON(&person, r.Body)
-	h.db.Post(person)
+	err := h.db.Post(person)
+	if err != nil {
+		h.l.Println("[ERROR] Getting all people: ", err.Error())
+		data.ToJSON(&GenericError{err.Error()}, rw)
+		return
+	}
 }
